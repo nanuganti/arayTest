9a8ccab07009ae66aa30a0e85bd637a74b19c93c
Narahari Anuganti <nanuganti@ebsco.com>
2022-04-05 18:43:54 -0400

    feat: add query parameters

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
-const port = 3034;
+const port = 3035;
 const WordMatching = require("./WordMatching");
 
 app.use("/", (req, res) => {
-    let service = new WordMatching("abc", "abz");
+    const { firstWord, secondWord } = req.query;
+    let service = new WordMatching(firstWord, secondWord);
     const response = service.getMatchingScore();
     res.send(`returning scrore is ${response} `, 200);
 })
 
 app.listen(port, () => {
-    console.log("app running on port 3034");
+    console.log("app running on port 3035");
 })
